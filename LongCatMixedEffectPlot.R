bf08406f72ca7935a7b6569eaeab114a871da851
Koen Vanbrabant <kn.vanbrabant@gmail.com>
2016-12-08 09:27:15 +0100

    changes  the n-table

--- LongCatMixedEffectPlot.R
+++ LongCatMixedEffectPlot.R
@@ -1,15 +1,19 @@
 ##########################################################################################################################
 # Function LongCatMixedEffectPlot: makes a plot of the mean of multiple groups when time is categorical
 # Author: koen.vanbrabant@kuleuven.be
-# date: 5/12/2016
+# date: 8/12/2016
 ###########################################################################################################################
+
 # dependencies
 library(ggplot2); theme_set(theme_bw())
 library(lme4)
 library(plyr)
+library(gtable)
+library(grid)
+library(Hmisc)
 
 # function
-LongCatMixedEffectPlot = function(fit,conf.level=.95,dodge.level=.70,n.placement=1.5){
+LongCatMixedEffectPlot = function(fit,conf.level=.95,dodge.level=.70){
   # get the data out of the model,droplevel and give name
   data = data.frame(fit@frame[,1],fit@frame[,2],fit@frame[,3],fit@frame[,4])
   names(data) = c('y','moment','condition','id')
@@ -35,34 +39,66 @@ LongCatMixedEffectPlot = function(fit,conf.level=.95,dodge.level=.70,n.placement
 
   # get number of observations for each measurement moment conditional on condition
   data$dummy_count = 1
-  n_table = ddply(data[!is.na(data$y),],c('condition','moment'),summarise,total=sum(dummy_count))
+  n_table = ddply(data[!is.na(data$y),],c('condition','moment'),summarise,total=sum(!is.na(y)))
   n_table$moment.cont = df_with_info$moment.cont
-  n_table$placement = range(df_with_info$y)[1] - (range(df_with_info$y)[2] - range(df_with_info$y)[1])/n.placement
 
   
-  # plotting
-  # get labels for plot
+  ##### plotting
+  ## get labels for mean plot
   plot_labels = list(
-    x = ifelse(label(fit@frame)[2]=='',names(fit@frame)[2],label(fit@frame)[2]),
+    x = ifelse(label(fit@frame)[2]=='','',label(fit@frame)[2]),
     y = ifelse(label(fit@frame)[1]=='',names(fit@frame)[1],label(fit@frame)[1]),
-    shape=''
-  )
-  
-  
-  # make the plot
-  ggplot() + 
+    shape='')
+  ##make mean plot
+  mean.plot = ggplot() + 
       geom_point(data=df_with_info,aes(y=y,x=moment.cont,shape=condition),
                                      position=position_dodge(width = dodge.level)) +
     geom_linerange(data=df_with_info,aes(ymin=lower_ci,ymax=upper_ci,x=moment.cont,group=condition),
-                   alpha=.5,position=position_dodge(width = dodge.level)) + scale_x_discrete(limits=df_with_info$moment) +
+                   alpha=.5,position=position_dodge(width = dodge.level)) + scale_x_discrete(limits=unique(fit@frame$moment)) +
+    labs(plot_labels) + theme_bw() + theme(legend.position='top')
   
-    geom_text(data=n_table,aes(x=moment.cont,y=placement,label=c(total),group=condition),
-                                    position = position_dodge(width = dodge.level)) +
-    labs(plot_labels) 
+  
+  # number of subjects
+  risk.plot = ggplot(n_table) + geom_text(aes(x=moment.cont,y=condition,label=total),size=3.5) +
+    theme_bw() + 
+    theme(axis.text.x = element_blank(), 
+          axis.title.x = element_blank(), axis.title.y = element_blank(),
+          axis.text.y = element_text(face="bold"),
+          axis.text.y = element_blank(), 
+          axis.title.y = element_blank(), axis.title.y = element_blank(),
+          axis.ticks = element_blank(),
+          panel.grid = element_blank(), panel.border = element_blank()
+    ) + theme(plot.title = element_text(hjust = 0)) 
+  
+  ## combine mean and risk.plot
+  both = rbind(ggplotGrob(mean.plot), ggplotGrob(risk.plot), size="last")
+  panels <- both$layout$t[grep("panel", both$layout$name)]
+  both$heights[panels[1]] = unit(1,"null") 
+  both$heights[panels[2]] = unit(2,"lines")
+  grid.newpage()
+  grid.draw(both)
 }
     
 
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
